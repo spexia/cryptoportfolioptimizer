5b322eeaa109a87c3f9c7855c266516bcd5684db
Kaan Akdede <148701422+spexia@users.noreply.github.com>
2024-02-01 19:33:58 +0300

    Add files via upload

--- gethistoricaldata.py
+++ gethistoricaldata.py
@@ -1,6 +1,37 @@
-from binance import Client
-import config
 import csv
+from binance.client import Client
+import time
+import config
 
 client = Client(config.apiKey, config.secretKey)
-symbolList = input("Please enter a symbol : (BTCUSDT)")
+
+def write(symbol, candlesticks):
+    csvFileW = open(symbol + "_historical_data.csv", "w", newline="")
+    klines_writer = csv.writer(csvFileW, delimiter=",")
+
+    for candlestick in candlesticks:
+        klines_writer.writerow(candlestick)
+
+    csvFileW.close()
+
+def get_historical_data():
+    symbols = input("Please enter symbols (BTC,ETH): ").split(' ')
+    startdate = input("Please enter start date (November 3, 2023): ")
+    enddate = input("Please enter end date (November 5, 2023): ")
+    candletime = input("Please enter time interval (1): ")
+
+    time.sleep(2)
+
+    print(f"Selected symbols are: {symbols}\nstartdate: {startdate}, enddate: {enddate}\ntime interval: {candletime} MINUTE")
+
+    time.sleep(1)
+    
+    for symbol in symbols:
+        print("FETCHING DATA FOR: ", symbol)
+        symbol_with_usdt = symbol + "USDT"
+
+        kline_interval = getattr(Client, f"KLINE_INTERVAL_{candletime}MINUTE")
+
+        candlesticks = client.get_historical_klines(symbol_with_usdt, kline_interval, startdate, enddate)
+        write(symbol, candlesticks)
+        print("Data written for: ", symbol)
---
+++ main.py
@@ -0,0 +1,15 @@
+from gethistoricaldata import get_historical_data
+import time
+
+
+print("==================================================")
+print("==================================================")
+print("Ｃｒｙｐｔｏ Ｐｏｒｔｆｏｌｉｏ Ｏｐｔｉｍｉｚｅｒ")
+print("==================================================")
+print("==================================================")
+print("made by spexia  https://github.com/spexia/cryptoportfolioptimizer\n")
+
+time.sleep(1)
+
+
+get_historical_data()
