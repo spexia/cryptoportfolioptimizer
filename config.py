3e8287611ac6c588b75f59f0f51b44c125264eeb
Kaan Akdede <148701422+spexia@users.noreply.github.com>
2024-02-03 18:22:42 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,5 @@
 apiKey = "Sj16b2MdofYWWBOGkvlng4NRVCG9gEdvLyOytTJscQz2feHfvYZUiFIkCpwDtTtZ"
 secretKey = "Tjw2dNO5zt7k8GHGyhLzAHwFlhLRxiKusMqy7SkMWiLRTXlXx9Oc0WmCncmChApb"
+interval = "Client.KLINE_INTERVAL_1MINUTE"
+start_time = "3 November, 2023"
+end_time = "4 November, 2023"
