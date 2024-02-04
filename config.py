7234969baf09658d738a78161a963e86a2235601
Kaan Akdede <148701422+spexia@users.noreply.github.com>
2024-02-04 16:09:31 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-apiKey = "Sj16b2MdofYWWBOGkvlng4NRVCG9gEdvLyOytTJscQz2feHfvYZUiFIkCpwDtTtZ"
-secretKey = "Tjw2dNO5zt7k8GHGyhLzAHwFlhLRxiKusMqy7SkMWiLRTXlXx9Oc0WmCncmChApb"
+apiKey = ""
+secretKey = ""
 interval = "Client.KLINE_INTERVAL_1MINUTE"
 start_time = "3 November, 2023"
 end_time = "4 November, 2023"
